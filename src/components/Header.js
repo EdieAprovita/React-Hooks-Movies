9df30f70ccc08a3d348b71b52be8d138b1cc87a0
EdieAprovita <edieveg316@gmail.com>
2020-08-13 21:23:59 -0500

    Basic app

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header className0='App-header'>
+      <h2>{props.text}</h2>
+    </header>
+  );
+};
+export default Header;
